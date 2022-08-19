dd96ca7ede3913df108b21ecd1fbabb2298e7557
rytonius <ryan3rdeye@gmail.com>
2022-08-18 19:31:07 -0800

    started 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map(book => book.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ];
+
+// function getThe(array) {
+//     return array.map(book => book.title);
+// }
+
+// getThe(books);
